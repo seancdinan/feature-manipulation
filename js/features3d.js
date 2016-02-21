421a29a393acb995120506c9c7cf4f9a73377328
Sean Dinan <seancdinan@gmail.com>
2016-02-21 18:30:14 -0500

    Rotation works! Lil wonky, but basic motion works. Probably just need to play with the mouse movement to camera movement ratio

--- js/features3d.js
+++ js/features3d.js
@@ -23,8 +23,9 @@ function pickEm(whichDropdown, whichBall){
 
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, testCanvas.width/testCanvas.height, 0.1, 1000);
-	var renderer = new THREE.WebGLRenderer({canvas: testCanvas});
+	var renderer = new THREE.WebGLRenderer({canvas: testCanvas, alpha: true});
 	document.body.appendChild(renderer.domElement);
+	renderer.setClearColor(0x444444)
 
 // Make some shadows
 var ambient = new THREE.AmbientLight(0x444444);
@@ -47,18 +48,82 @@ function render(){
 	renderer.render(scene, camera);
 }
 
-var geometry = new THREE.SphereGeometry(1, 20, 20);
+var geometry = new THREE.SphereGeometry(1.25, 20, 20);
 var ball1 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 var ball2 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 var ball3 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 var ball4 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 var ball5 = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 scene.add(ball1, ball2, ball3, ball4, ball5, light);
-ball1.position.x = -2; ball1.position.z = -4;
-ball2.position.x = -1; ball2.position.z = -2;
+ball1.position.x = -3; ball1.position.z = -5;
+ball2.position.x = -1.5; ball2.position.z = -2.5;
 ball3.position.x = 0; ball3.position.z = 0;
-ball4.position.x = 1; ball4.position.z = -2;
-ball5.position.x = 2; ball5.position.z = -4;
-camera.position.set(0,3,10);
+ball4.position.x = 1.5; ball4.position.z = -2.5;
+ball5.position.x = 3; ball5.position.z = -5;
+camera.position.set(0,0,10);
 render();
 
+
+//*************************************************
+//***************** Mouse Control   ***************
+//*************************************************
+var isMouseDown;
+var mouseDownPosition = [];
+var mouseMovement = [];
+var camX, camY;
+document.addEventListener('mousemove', onDocumentMouseMove);
+document.addEventListener('mousedown', function(event){
+	isMouseDown = true;
+	mouseDownPosition[0] = event.clientX;
+	mouseDownPosition[1] = event.clientY;
+	});
+document.addEventListener('mouseup', function(){isMouseDown = false;});
+
+
+function onDocumentMouseMove(event){
+	if (isMouseDown){
+		mouseMovement[0] = event.clientX;
+		mouseMovement[1] = event.clientY;
+		camX = (mouseDownPosition[0] - mouseMovement[0])/window.innerWidth;
+		camY = (mouseDownPosition[1] - mouseMovement[1])/window.innerHeight;
+		if (Math.abs(camera.position.x + camX) < 4){
+			camera.position.x += camX;
+		}
+		if (Math.abs(camera.position.y - camY) < 4){
+			camera.position.y -= camY;
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
