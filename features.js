66093cc2409e0224b8080ef82a302a0589d378d6
Sean Dinan <seancdinan@gmail.com>
2016-02-21 11:37:03 -0500

    Dropdowns all set to change color of each ball

--- features.js
+++ features.js
@@ -3,10 +3,15 @@ function colorDiv(target, color){
 	div.style.backgroundColor = color;
 }
 
-var colorOptions = ['hsl(310, 40%, 70%)', 'hsl(50, 100%, 60%)', 'hsl(120, 70%, 60%)', 'hsl(10, 50%, 50%)', 'hsl(180, 60%, 70%)'];
+colorDiv('ball_1', 'white')
+colorDiv('ball_2', 'white')
+colorDiv('ball_3', 'white')
+colorDiv('ball_4', 'white')
+colorDiv('ball_5', 'white')
 
-colorDiv('ball_1',colorOptions[0])
-colorDiv('ball_2',colorOptions[1])
-colorDiv('ball_3',colorOptions[2])
-colorDiv('ball_4',colorOptions[3])
-colorDiv('ball_5',colorOptions[4])
+function pickEm(whichDropdown, whichBall){
+	var selection = document.getElementById(whichDropdown);
+	var colorValue = selection.options[selection.selectedIndex].value;
+	colorDiv(whichBall, colorValue);
+};
+
